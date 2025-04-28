bad871272d47d35e567750b0d401a969fb051392
Disha Shivhare <dishashivhare44@gmail.com>
2025-04-28 19:41:42 +0530

    exporting multiple local modules

---
+++ FS_Module/Local_Modules/Example2/welcome.js
@@ -0,0 +1,9 @@
+function greet(message)
+{
+    console.log("Hello",message)
+}
+const PI=3.14
+const arr=[19,34,65,24,27,98]
+module.exports.greet=greet
+module.exports.PI=PI
+module.exports.arr=arr
