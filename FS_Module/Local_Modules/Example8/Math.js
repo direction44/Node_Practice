ea6bc79011a218c7d1de05597f15f44d6f64ee91
Disha Shivhare <dishashivhare44@gmail.com>
2025-04-28 20:23:45 +0530

    export using exports keyword from local module

---
+++ FS_Module/Local_Modules/Example8/Math.js
@@ -0,0 +1,12 @@
+function add(a,b)
+{
+    return a+b
+}
+
+function product(a,b)
+{
+    return a*b
+}
+
+exports.add=add
+exports.product=product
---
+++ FS_Module/Local_Modules/Example8/main.js
@@ -0,0 +1,3 @@
+const Math = require('./Math.js');
+console.log("Additon of 10,20 is:",Math.add(10,20))
+console.log("Product of 10,20 is:",Math.product(10,20))
