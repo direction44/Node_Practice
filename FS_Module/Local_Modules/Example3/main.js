d9708eb3b646f2728668d845efed15056466c006
Disha Shivhare <dishashivhare44@gmail.com>
2025-04-28 19:44:59 +0530

    exporting multiple local modules way 2

---
+++ FS_Module/Local_Modules/Example3/main.js
@@ -0,0 +1,8 @@
+const welcome = require('./welcome.js');
+welcome.greeting("Disha")
+console.log(welcome.mPI)
+console.log(welcome.mArr)
+for(arr of welcome.mArr)
+{
+    console.log(arr)
+}
---
+++ FS_Module/Local_Modules/Example3/welcome.js
@@ -0,0 +1,7 @@
+function greet(message)
+{
+    console.log("Hello",message)
+}
+const PI=3.14
+const arr=[19,34,65,24,27,98]
+module.exports={greeting:greet,mPI:PI,mArr:arr}
