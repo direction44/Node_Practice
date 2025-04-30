aaea99ec1f9ee169b89084a540b0f81c661a76e8
Disha Shivhare <dishashivhare44@gmail.com>
2025-04-30 20:40:13 +0530

    use third party chalk module in node

---
+++ FS_Module/chalk_module/Example1/main.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+console.log(chalk.red("Hello Disha"))
+console.log(chalk.yellow.underline("Good evening"))
+console.log(chalk.yellow.bgGreen("Welcome to node js"))
+console.log(chalk.blueBright.bgGray.underline.strikethrough("You are using chalk module"))
+console.log(chalk.blueBright.bgGray.inverse("Here background will be blue and text colour should be in gray"))
