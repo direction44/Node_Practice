a5d0b020a9eb1c2ac1060fad922300b780e3cc81
Disha Shivhare <dishashivhare44@gmail.com>
2025-05-03 22:41:36 +0530

    initial commit

---
+++ FS_Module/QR Generator Project/qrcodegen.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs"
+import fspromises from "fs/promises"
+import chalk from "chalk";
+import validator from "validator"
+
+async function getURL(){
+
+}
+
+async function getImageType() {
+    
+}
+
+async function generateQRCode (url,imageType) {
+    
+}
+
+async function writeToFile(filePath,url) {
+    
+}
+
+async function doTask() {
+    
+}
