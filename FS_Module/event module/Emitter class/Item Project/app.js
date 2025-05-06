b0199f4fe64d17c9cf5184650a770c8f6b15da18
Disha Shivhare <dishashivhare44@gmail.com>
2025-05-06 22:29:31 +0530

    custom events example 2 modified

--- FS_Module/event module/Emitter class/Item Project/app.js
+++ FS_Module/event module/Emitter class/Item Project/app.js
@@ -2,6 +2,8 @@ const Item = require('./EmitterItems.js');
 
 let item=new Item("buiscuit",100)
 
+console.log(`Item Name ${item.Name}`)
+console.log(`Item Price ${item.Price}`)
 item.on("priceChanged",(newDetails)=>{
 console.log(`Items Name:${newDetails.name},Old Price is:${newDetails.oldPrice},New Price is:${newDetails.newPrice},Percentage Increased by:${newDetails.raisePer}%`)
 })
